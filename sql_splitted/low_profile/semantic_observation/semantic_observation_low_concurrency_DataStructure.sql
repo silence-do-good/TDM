916fa183c8c1f27a46d3288a09cca7f168e089b6
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-05-05 02:17:56 -0700

    splitted sql 2

---
+++ sql_splitted/low_profile/semantic_observation/semantic_observation_low_concurrency_DataStructure.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: occupancy; Type: TABLE DATA; Schema: public; Owner: benchmark
+--
+
+INSERT INTO occupancy VALUES ('377f69a3-2bbc-4f50-a2c5-3f74b68149a1', '3068', 65, '2017-11-08 00:00:00', 'vSensor2');
+
+
+--
+-- Data for Name: presence; Type: TABLE DATA; Schema: public; Owner: benchmark
+--
+
+INSERT INTO presence VALUES ('7b8904aa-6756-47d4-9ab7-aef56f892f2e', '4064adbc_f93d_4725_bd2b_c83e3fc8eb92', '1425', '2017-11-21 13:00:00', 'vSensor1');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
