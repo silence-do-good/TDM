ef429dfdb99808b9c4fb12598a2d59d5244cc89a
xnishx <nishanthnarayan027@gmail.com>
2018-05-07 10:56:55 -0700

    test
    int

--- src/Simulator.java
+++ src/Simulator.java
@@ -3,6 +3,7 @@ import java.util.Timer;
 public class Simulator {
 
 	public static void main(String[] args) {
+		int a;
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new SimulatorTask(timer), 0, 125); //repeat every 125ms
 		System.out.println("Simulator is started.");
