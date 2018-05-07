1f4348ca47e4bb4de84d8fbf486da6c1b9919295
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-05-07 12:40:34 -0700

    insert sql

--- sql/insert_thermometerobservation.sql
+++ sql/insert_thermometerobservation.sql
@@ -1,1 +1,1 @@
-insert into select thermometerobservation(id, temperature, timeStamp, sensor_id) values (?,?,?,?);
+insert into thermometerobservation(id, temperature, timeStamp, sensor_id) values (?,?,?,?);
