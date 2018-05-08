927447c32c619ce674175f624376b6fac7163101
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-05-08 12:44:21 -0700

    delete sql_splitted

--- sql_splitted/run_mysql.ps1
+++
@@ -1,8 +0,0 @@
-$i = 0
-foreach ($f in Get-ChildItem -path "." -Filter "*.sql" )
-{
-    $i++
-    Write-Host $i
-    Write-Host $f
-    cmd.exe /c "mysql.exe --user=root --password=1004 low_profile < $f >> output.txt"
-}
